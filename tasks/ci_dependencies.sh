b3c577313ca74e0decc941926734609fb9b909be
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-03-08 17:04:19 -0500

    fixup paths

--- tasks/ci_dependencies.sh
+++ tasks/ci_dependencies.sh
@@ -4,9 +4,9 @@ case $CIRCLE_NODE_INDEX in
 
     # image tests
     0)
-        eval $(node docker.js pull)
+        eval $(node tasks/docker.js pull)
         npm run cibuild
-        eval $(node docker.js run)
+        eval $(node tasks/docker.js run)
         eval $(node tasks/docker.js setup)
         ;;
 
