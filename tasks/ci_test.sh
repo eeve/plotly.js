751c877c17092decfc03c53872991a792306e9e8
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-03-08 18:49:12 -0500

    try long ass CMDs

--- tasks/ci_test.sh
+++ tasks/ci_test.sh
@@ -4,17 +4,12 @@ case $CIRCLE_NODE_INDEX in
 
     # image tests
     0)
-        npm run test-image
-        npm run test-image-gl2d
-        npm run test-export
-        npm run test-syntax
-        npm run lint
+        npm run test-image && npm run test-image-gl2d && npm run test-export && npm run test-syntax && npm run lint
         ;;
 
     # jasmine tests
     1)
-        npm run citest-jasmine
-        npm run test-bundle
+        npm run citest-jasmine && npm run test-bundle
         ;;
 
 esac
