131acd5c2bc18cc0cf2c032665da2eaf32d8a2f7
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-03-08 17:28:16 -0500

    try no-docker-run

--- tasks/ci_test.sh
+++ tasks/ci_test.sh
@@ -4,17 +4,14 @@ case $CIRCLE_NODE_INDEX in
 
     # image tests
     0)
-        npm run test-image
-        npm run test-image-gl2d
-        npm run test-export
+        npm run test-syntax
+        eslint .
         ;;
 
     # jasmine tests
     1)
         npm run citest-jasmine
         npm run test-bundle
-        npm run test-syntax
-        eslint .
         ;;
 
 esac
